39de4e8ea3a17ab8fa37ffcb60653f58c536ab0d
Hugh O'Brien <git@hughobrien.ie>
2020-04-07 21:05:49 +0100

    ingress-nginx it is

--- provision-post.sh
+++ provision-post.sh
@@ -17,32 +17,42 @@ one_off_manifest=${2:-""}
 	exit 0
 }
 
+ingress_nginx_ver="nginx-0.30.0"
+ingress_nginx_manifest_url1="https://raw.githubusercontent.com/kubernetes/ingress-nginx/${ingress_nginx_ver}/deploy/static/mandatory.yaml"
+ingress_nginx_manifest_url2="https://raw.githubusercontent.com/kubernetes/ingress-nginx/${ingress_nginx_ver}/deploy/static/provider/baremetal/service-nodeport.yaml"
+
 longhorn_ver="v0.8.0"
-longhorn_manifest="https://raw.githubusercontent.com/longhorn/longhorn/${longhorn_ver}/deploy/longhorn.yaml"
+longhorn_manifest_url="https://raw.githubusercontent.com/longhorn/longhorn/${longhorn_ver}/deploy/longhorn.yaml"
 
 certmanager_ver="v0.14.1"
-certmanager_manifest="https://github.com/jetstack/cert-manager/releases/download/${certmanager_ver}/cert-manager.yaml"
+certmanager_manifest_url="https://github.com/jetstack/cert-manager/releases/download/${certmanager_ver}/cert-manager.yaml"
 
 # need to pre-create these so we can restore the TLS certs to them
 for namespace in prometheus longhorn-system; do
 	$kubectl create namespace "$namespace" || true
 done
 
-for service in secrets/*-cert.yaml; do
-	[ -e "${service}" ] && $kaf < "${service}"
+for tls_cert in secrets/*-cert.yaml; do
+	$kaf < "${tls_cert}"
 done
 
 for url in \
-	"$longhorn_manifest" \
-	"$certmanager_manifest"; do
+	"$ingress_nginx_manifest_url1" \
+	"$ingress_nginx_manifest_url2" \
+	"$longhorn_manifest_url" \
+	"$certmanager_manifest_url"; do
 	curl --location --silent "$url" | $kaf
 done
+
+# make ingress a LB so that svclb picks it up
+$kubectl patch service -n ingress-nginx ingress-nginx -p \''{"spec":{"type":"LoadBalancer"}}'\'
 
 # ensure certmanager is ready
 while [ "$($kubectl get pods -n cert-manager -l app=webhook -o json | jq '.items[0].status.containerStatuses[0].ready')" != true ]; do
 	sleep 5
 done
 
+# install the rest
 for f in manifests/*; do
 	$kaf < "$f"
 done
