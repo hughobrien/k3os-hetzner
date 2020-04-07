4a8237a13546fdf87ec814a6f8d76bc639d4831e
Hugh O'Brien <git@hughobrien.ie>
2020-04-07 17:44:34 +0100

    config traefik helm in hackey way

--- provision-post.sh
+++ provision-post.sh
@@ -7,7 +7,8 @@ k3os_user=rancher
 ssh_key=secrets/ssh-terraform
 ssh_opts="-o StrictHostKeyChecking=no"
 
-kubectl="ssh $ssh_opts -i $ssh_key ${k3os_user}@${node_ipv4_public} kubectl"
+ssh="ssh $ssh_opts -i $ssh_key ${k3os_user}@${node_ipv4_public}"
+kubectl="$ssh kubectl"
 kaf="$kubectl apply -f -"
 
 one_off_manifest=${2:-""}
@@ -27,6 +28,14 @@ while [ "$($kubectl get configmap -n kube-system traefik | wc -l | xargs)" != 2 
 	sleep 5
 done
 
+for namespace in prometheus longhorn-system; do
+	$kubectl create namespace "$namespace" || true
+done
+
+for service in secrets/*-cert.yaml; do
+	[ -e "${service}" ] && $kaf < "${service}"
+done
+
 for url in \
 	"$longhorn_manifest" \
 	"$certmanager_manifest"; do
@@ -38,14 +47,7 @@ while [ "$($kubectl get pods -n cert-manager -l app=webhook -o json | jq '.items
 	sleep 5
 done
 
-# shellcheck disable=SC2043
-for namespace in prometheus; do
-	$kubectl create namespace "$namespace"
-done
-
-for service in secrets/*-cert.yaml; do
-	[ -e "${service}" ] && $kaf < "${service}"
-done
+$ssh 'sudo tee /var/lib/rancher/k3s/server/manifests/traefik.yaml' < traefik.yaml
 
 for f in manifests/*; do
 	$kaf < "$f"
