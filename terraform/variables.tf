bb1c960238f228c2e7a4703fb9c24aabd0ce5695
Hugh O'Brien <git@hughobrien.ie>
2020-04-11 00:32:51 +0100

    bump k3s

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,5 +4,5 @@ variable "default_image" { default = "debian-10" }
 variable "default_rescue" { default = "linux64" }
 variable "master_server_type" {}
 variable "node_server_type" {}
-variable "default_k3os_ver" { default = "v0.9.1" }
+variable "default_k3os_ver" { default = "v0.10.0" }
 variable "default_cidr_offset" { default = 2 }
