738eadbfbab3342651caf61233437ecae94cf181
Hugh O'Brien <git@hughobrien.ie>
2020-04-11 02:39:00 +0100

    add docker registry cred generation

--- configure-remote.sh
+++ configure-remote.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
 
+# shellcheck disable=SC2016
+jq_ns_template='{"apiVersion": "v1", "kind": "Namespace", "metadata": {"name": $ns}}'
+
 longhorn_ver="v0.8.0"
 longhorn_manifest_url="https://raw.githubusercontent.com/longhorn/longhorn/${longhorn_ver}/deploy/longhorn.yaml"
 
@@ -17,28 +20,50 @@ longhorn_cert="secrets/longhorn-cert.yaml"
 kubectl apply -f manifests/longhorn-ingress-storageclass.yaml
 
 # docker registry
-kubectl create namespace docker-registry
-docker_registry_cert="secrets/docker-registry-cert.yaml"
+jq -n --arg ns docker-registry "$jq_ns_template" | kubectl apply -f -
+docker_registry_cert="secrets/registry-cert.yaml"
 [ -f "$docker_registry_cert" ] && kubectl apply -f "$docker_registry_cert"
+
+docker_registry_htpasswd="secrets/registry-htpasswd.yaml"
+docker_registry_credentials="secrets/registry-credentials.yaml"
+
+if [ ! -f "$docker_registry_htpasswd" ]; then
+	docker_registry_password="secrets/registry.pw"
+	docker_registry_user="k3s"
+	openssl rand -out "$docker_registry_password" -hex 32
+
+	kubectl create secret generic -n docker-registry \
+		"$(basename $docker_registry_htpasswd)" \
+		--dry-run=client -o yaml \
+		--from-file=htpasswd=<(htpasswd -Bin "$docker_registry_user" < "$docker_registry_password") \
+		> "$docker_registry_htpasswd"
+	kubectl apply -f "$docker_registry_htpasswd"
+
+	kubectl create secret docker-registry \
+		"$(basename $docker_registry_credentials)" \
+		--docker-server=registry.k3s.hughobrien.ie \
+		--docker-username="$docker_registry_user" \
+		--docker-password="$(cat "$docker_registry_password")" \
+		--dry-run=client -o yaml > "$docker_registry_credentials"
+	kubectl apply -f "$docker_registry_credentials"
+fi
 kubectl apply -f manifests/docker-registry.yaml
 
-TODO docker regcred setup
-
 # prometheus
-kubectl create namespace prometheus
+jq -n --arg ns prometheus "$jq_ns_template" | kubectl apply -f -
 prometheus_cert="secrets/prometheus-cert.yaml"
 [ -f "$prometheus_cert" ] && kubectl apply -f "$prometheus_cert"
 kubectl apply -f manifests/prometheus.yaml
 
 # argo workflows
-kubectl create namespace argo
+jq -n --arg ns argo "$jq_ns_template" | kubectl apply -f -
 kubectl apply -n argo -f "$argo_workflows_manifest_url"
 argo_cert="secrets/argo-cert.yaml"
 [ -f "$argo_cert" ] && kubectl apply -f "$argo_cert"
 kubectl apply -f manifests/argo-ingress.yaml
 
 # argo cd
-kubectl create namespace argocd
+jq -n --arg ns argocd "$jq_ns_template" | kubectl apply -f -
 kubectl apply -n argocd -f "$argo_cd_manifest_url"
 # use argo in open mode
 kubectl patch deploy -n argocd argocd-server --type json \
