173f2740fe0bab74c6e738918c758482ed4e8116
Hugh O'Brien <git@hughobrien.ie>
2020-04-11 19:34:09 +0100

    cleaner backup script

--- backup.sh
+++ backup.sh
@@ -1,17 +1,30 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
 
-k="kubectl get secret -o yaml"
-strip="yq -Y 'del(.metadata.resourceVersion) | del(.metadata.annotations."kubectl.kubernetes.io/last-applied-configuration") | del(.metadata.uid)'"
+get_secret() {
+	namespace="$1"
+	secret_name="$2"
+
+	[ -f "secrets/${secret_name}" ] && {
+		echo "$secret_name" exists, skipping
+		return 0
+	}
+
+	kubectl get secret -n "$namespace" "$secret_name" -o yaml \
+	| yq -Y 'del(.metadata.resourceVersion)' \
+	| yq -Y 'del(.metadata.uid)' \
+	| yq -Y 'del(.metadata.annotations."kubectl.kubernetes.io/last-applied-configuration")' \
+	> "secrets/${secret_name}.yaml"
+}
 
 mkdir -p secrets
-cd secrets
 
-$k -n argo argo-cert | $strip > argo-cert.yaml
-$k -n argocd argocd-cert | $strip > argocd-cert.yaml
-$k -n default k3s-cert | $strip > k3s-cert.yaml
-$k -n default registry-credentials | $strip > registry-credentials.yaml
-$k -n docker-registry registry-htpasswd | $strip > registry-htpasswd.yaml
-$k -n docker-registry registry-cert | $strip > registry-cert.yaml
-$k -n longhorn-system longhorn-cert | $strip > longhorn-cert.yaml
-$k -n prometheus prometheus-cert | $strip > prometheus-cert.yaml
+get_secret argo argo-cert
+get_secret argocd argocd-cert
+get_secret argocd argocd-cert
+get_secret default k3s-cert
+get_secret default registry-credentials
+get_secret docker-registry registry-htpasswd
+get_secret docker-registry registry-cert
+get_secret longhorn-system longhorn-cert
+get_secret prometheus prometheus-cert
