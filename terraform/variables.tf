276fdd3ec118cd5d409f18aee534179d64b43e67
Hugh O'Brien <git@hughobrien.ie>
2020-04-12 19:24:24 +0100

    refactor tf vars

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,8 +1,9 @@
-variable "network" { type = map(string) }
-variable "presence" { type = list(map(string)) }
-variable "default_image" { default = "debian-10" }
-variable "default_rescue" { default = "linux64" }
-variable "master_server_type" {}
-variable "node_server_type" {}
-variable "default_k3os_ver" { default = "v0.10.0" }
 variable "default_cidr_offset" { default = 2 }
+variable "default_image" { default = "debian-10" }
+variable "default_k3os_ver" { default = "v0.10.0" }
+variable "default_rescue" { default = "linux64" }
+variable "fqdn" { default = "k3s.hughobrien.ie" }
+variable "master_server_type" {}
+variable "network" { type = map(string) }
+variable "node_server_type" {}
+variable "presence" { type = list(map(string)) }
